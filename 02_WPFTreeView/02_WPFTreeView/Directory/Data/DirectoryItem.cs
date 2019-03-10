0b4e1463eb4f595294679f0569e7d0966ba7b0fb
redcican <cican17@gmail.com>
2019-03-11 00:21:09 +0100

    Add new folder and classes.

---
+++ 02_WPFTreeView/02_WPFTreeView/Directory/Data/DirectoryItem.cs
@@ -0,0 +1,11 @@
+﻿
+namespace _02_WPFTreeView.Directory.Data
+{
+    /// <summary>
+    /// Information about a diretory item such as a drive, a file or a folder
+    /// </summary>
+    public class DirectoryItem
+    {
+
+    }
+}
