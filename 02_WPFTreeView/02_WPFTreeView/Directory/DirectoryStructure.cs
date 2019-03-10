f2799a62fff3fabb2c685d4812ad2890d634859d
redcican <cican17@gmail.com>
2019-03-10 23:32:40 +0100

    Learn basic WPF MVVM, add a helper class to query information about directories.

---
+++ 02_WPFTreeView/02_WPFTreeView/Directory/DirectoryStructure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_WPFTreeView.Directory
+{
+    /// <summary>
+    /// A helper class to query information about directories
+    /// </summary>
+    public static class DirectoryStructure
+    {
+
+    }
+}
