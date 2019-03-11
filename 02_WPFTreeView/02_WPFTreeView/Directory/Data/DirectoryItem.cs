d21f419984c58f22fd6296aa09819d8bcd9ec4b9
redcican <cican17@gmail.com>
2019-03-11 10:42:44 +0100

    Move the helper function "GetFileFolderName" from the MainWindow.cs to the DirectoryStructure.cs.

--- 02_WPFTreeView/02_WPFTreeView/Directory/Data/DirectoryItem.cs
+++ 02_WPFTreeView/02_WPFTreeView/Directory/Data/DirectoryItem.cs
@@ -1,11 +1,15 @@
 ﻿
-namespace _02_WPFTreeView.Directory.Data
+namespace _02_WPFTreeView
 {
     /// <summary>
     /// Information about a diretory item such as a drive, a file or a folder
     /// </summary>
     public class DirectoryItem
     {
+        /// <summary>
+        /// The absolute path of this item
+        /// </summary>
+        public string FullPath { get; set; }
 
     }
 }
