26d2a8e4996a813e4b91bdcbf45f7007fbd98381
redcican <cican17@gmail.com>
2019-03-11 15:30:36 +0100

    Add a view model for the directory structure on the machine.

---
+++ 02_WPFTreeView/02_WPFTreeView/Directory/ViewModels/DirectoryStructureViewModel.cs
@@ -0,0 +1,29 @@
+﻿using _02_WPFTreeView.Directory.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace _02_WPFTreeView
+{
+    /// <summary>
+    /// The view model for the application main Directory view
+    /// </summary>
+    class DirectoryStructureViewModel:BaseViewModel
+    {
+        #region Public Property
+        /// <summary>
+        /// A list of all directories on the machines
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Items { get; set; }
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DirectoryStructureViewModel()
+        {
+
+        }
+        #endregion
+    }
+}
