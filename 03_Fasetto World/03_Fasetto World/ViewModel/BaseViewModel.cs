5f53da59cf5c60db28556e2e7ee651ec46594808
redcican <cican17@gmail.com>
2019-03-12 16:16:13 +0100

    Add the logo files to the app and add the fody references to the project.

---
+++ 03_Fasetto World/03_Fasetto World/ViewModel/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace _03_Fasetto_World
+{
+    /// <summary>
+    /// A base view model that fires Property changed events as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e)=> { };
+    }
+
+
+}
