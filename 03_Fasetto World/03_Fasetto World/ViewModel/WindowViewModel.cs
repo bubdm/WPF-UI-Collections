2e33e9b47cc3bb1d3b973440b3f6b4b6ff0b76ca
redcican <cican17@gmail.com>
2019-03-13 10:04:05 +0100

    create a view model to contrl the main window.

---
+++ 03_Fasetto World/03_Fasetto World/ViewModel/WindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Windows;
+
+namespace _03_Fasetto_World
+{
+    /// <summary>
+    /// The view model for the custom flat window
+    /// </summary>
+    class WindowViewModel :BaseViewModel
+    {
+        #region
+        /// <summary>
+        /// the window this view model controls
+        /// </summary>
+        private readonly Window mWindow;
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public WindowViewModel(Window window)
+        {
+            window = mWindow;
+        }
+        #endregion
+    }
+}
