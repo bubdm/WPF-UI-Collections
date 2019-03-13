c2dd03ead22ddf01354e016622be45293b3f2447
redcican <cican17@gmail.com>
2019-03-13 16:04:47 +0100

    add the border and the main content to the window.

--- 03_Fasetto World/03_Fasetto World/ViewModel/WindowViewModel.cs
+++ 03_Fasetto World/03_Fasetto World/ViewModel/WindowViewModel.cs
@@ -12,34 +12,86 @@ namespace _03_Fasetto_World
         /// <summary>
         /// The window this view model controls
         /// </summary>
-        private Window mWindow;
+        private  Window mWindow;
         /// <summary>
         /// The margin around the window to allow for a drop shadow
         /// </summary>
-        private int mOuterMarginSize = 10;
+        private  int mOuterMarginSize = 10;
         /// <summary>
         /// The radius of the edges of the window.
         /// </summary>
-        private int mWindwoRadius = 10;
+        private  int mWindowRadius = 10;
         #endregion
 
         #region public properties
 
-        /// <summary>
+        /// <summary>   
         /// The size of the resize border around the window
         /// </summary>
         public int ResizeBorder { get; set; } = 6;
+        /// <summary>
+        /// The size of the resize border around the window, taking into account the outer margin
+        /// </summary>
+        public Thickness ResizeBorderThickness { get {return new Thickness(ResizeBorder +  mOuterMarginSize); } }
 
-        public Thickness ResizeBorderThickness { get {return new Thickness(ResizeBorder); } }
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set => mOuterMarginSize = value;
+        }
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        /// <summary>
+        ///The radius of the edges of the window.
+        /// </summary>
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set => mWindowRadius = value;
+        }
+
+        /// <summary>
+        ///The radius of the corners of the window.
+        /// </summary>
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
         #endregion
+        /// <summary>
+        /// The height of the title bar / caption of the window
+        /// </summary>
+        public int TitleHeight { get; set; } = 42;
 
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight+ResizeBorder); } }
         #region Constructor
         /// <summary>
         /// Default constructor
         /// </summary>
         public WindowViewModel(Window window)
         {
-            window = mWindow;
+            mWindow = window;
+
+            // Listen out for the window resizing
+            mWindow.StateChanged += (sender, e) =>
+            {
+                // Fire off events for all properties that are affected by a resize
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
         }
         #endregion
     }
