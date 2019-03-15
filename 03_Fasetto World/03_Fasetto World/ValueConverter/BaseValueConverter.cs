33b8bb0e7f9cdc8f9c4f68af11f927117c022329
redcican <cican17@gmail.com>
2019-03-15 16:10:29 +0100

    implement the convert method to convert any value to a page.

--- 03_Fasetto World/03_Fasetto World/ValueConverter/BaseValueConverter.cs
+++ 03_Fasetto World/03_Fasetto World/ValueConverter/BaseValueConverter.cs
@@ -1,21 +1,59 @@
-﻿
-using System;
+﻿using System;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace _03_Fasetto_World
 {
+    /// <summary>
+    /// A base value converter that allows direct XAML usage
+    /// </summary>
+    /// <typeparam name="T">The type of this value converter</typeparam>
     public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+        where T:class, new()
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        #region Private Members
+        /// <summary>
+        /// a single static instance of this value converter
+        /// </summary>
+        private static T mConverter = null;
+        #endregion
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        #region Markup Extension Methods
+        /// <summary>
+        /// Provides a static instance of the value converter
+        /// </summary>
+        /// <param name="serviceProvider">the service provider</param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            throw new NotImplementedException();
+            return mConverter ?? (mConverter = new T());
         }
+        #endregion
+
+        #region Value Converter Method
+
+        /// <summary>
+        /// The method to convert one type to another
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+
+        /// <summary>
+        /// The method to convert a value back to it's source type
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        #endregion
     }
 }
