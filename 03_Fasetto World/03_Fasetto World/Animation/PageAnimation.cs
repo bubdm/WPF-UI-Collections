2cdac6ca4fda7d5c009aefcd8651f549e9996069
redcican <cican17@gmail.com>
2019-03-22 15:16:04 +0100

    Add a default enum for page animation.

---
+++ 03_Fasetto World/03_Fasetto World/Animation/PageAnimation.cs
@@ -0,0 +1,22 @@
+﻿
+namespace _03_Fasetto_World
+{ 
+    /// <summary>
+    /// Styles of page animations for appearing/disappearing
+    /// </summary>
+    public enum PageAnimation
+    {
+        /// <summary>
+        /// No animations take place
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// The page slides in and fades in from the right
+        /// </summary>
+        SlideAndFadeInFromRight = 1,
+        /// <summary>
+        /// The page slides out and fades out to the left
+        /// </summary>
+        SlideAndFadeOutToLeft=2
+    }
+}
---
+++ 03_Fasetto World/03_Fasetto World/Pages/BasePage.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Windows.Controls;
+
+namespace _03_Fasetto_World
+{
+    /// <summary>
+    /// A base page for all pages to gain base functionaity
+    /// </summary>
+    public class BasePage:Page
+    {
+
+    }
+}
